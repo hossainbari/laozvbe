6826777b2870c9bb9cdbad555fbfc11f7ddeb365
laozvbe <laozvbe@a0827234-f371-11de-88d6-afc75ad17f39>
2010-01-06 05:00:10 +0000

    aaa

---
+++ src/abc/ab/dis/affss.java
@@ -0,0 +1,16 @@
+package abc.ab.dis;
+/*
+ * 소스 예제
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class affss extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
